7442fe5eea933c4a0dcb0130f6bc4b44c40d1942
Valney Filho <valneyf.dev@gmail.com>
2023-07-01 12:49:06 -0300

    Build chat server admin models

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -14,7 +14,7 @@ class Server(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='server_owner')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='server_category')
-    description = models.CharField(max_length=250, null=True)
+    description = models.CharField(max_length=250, blank=True, null=True)
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
