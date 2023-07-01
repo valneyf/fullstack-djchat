4cca485898524c339789400585e9538a3e9f29f9
Valney Filho <valneyf.dev@gmail.com>
2023-07-01 16:44:05 -0300

    Created api endpoint for filtering server by quantity

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -11,9 +11,13 @@ class ServerListViewSet(viewsets.ViewSet):
 
     def list(self, request):
         category = request.query_params.get('category')
+        qty = request.query_params.get('qty')
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
