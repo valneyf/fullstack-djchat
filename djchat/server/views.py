ff7b8c9d958d49194bf89da3807e27a1a3b7c8aa
Valney Filho <valneyf.dev@gmail.com>
2023-07-02 00:55:53 -0300

    Created filtering servers and returning annotation

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,9 +1,10 @@
+from django.db.models import Count
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
-from .models import Server
+from .models import Channel, Server
 from .serializer import ServerSerializer
 
 
@@ -15,6 +16,7 @@ class ServerListViewSet(viewsets.ViewSet):
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if by_user or by_serverid and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -25,6 +27,9 @@ class ServerListViewSet(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
         if qty:
             self.queryset = self.queryset[: int(qty)]
