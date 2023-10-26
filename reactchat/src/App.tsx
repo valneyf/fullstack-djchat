a6325bee59672cb429c010f892a57147e918599d
Valney Filho <valneyf.dev@gmail.com>
2023-10-26 11:42:57 -0300

    add:Included mui theme

--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -1,5 +1,7 @@
+import { ThemeProvider } from '@emotion/react';
 import Home from './pages/Home'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import createMuiTheme from './theme/theme';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -10,7 +12,13 @@ const router = createBrowserRouter(
 )
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  const theme = createMuiTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- reactchat/src/pages/Home.tsx
+++ reactchat/src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import { Box, CssBaseline } from "@mui/material"
+import PrimaryAppBar from "./templates/PrimaryAppBar"; 
 
 const Home = () => {
-    return <Box sx={{display:'flex'}}>
-        <CssBaseline />
-        Home
-        </Box>;
+    return ( 
+        <Box sx={{display:'flex'}}>
+            <CssBaseline />
+            <PrimaryAppBar />
+            Home
+        </Box>
+    );
 };
 
 export default Home;
